943ca1134fcb11b9bc05086656c0f1d9abd33300
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-08-29 02:43:25 +0200

    Clean

--- Sources/Git2JsonCore/Extensions/String+split.swift
+++ Sources/Git2JsonCore/Extensions/String+split.swift
@@ -2,16 +2,16 @@ import Foundation
 
 extension String {
     func filteredComponents<T>(separatedBy separator: T) -> [String] where T : StringProtocol {
-        return self.components(separatedBy: separator).cleanComponents()
+        return self.components(separatedBy: separator).cleanAndFilter()
     }
 
     func filteredComponents(separatedBy separators: CharacterSet) -> [String] {
-        return self.components(separatedBy: separators).cleanComponents()
+        return self.components(separatedBy: separators).cleanAndFilter()
     }
 }
 
 private extension Array where Element == String {
-    func cleanComponents() -> [String] {
+    func cleanAndFilter() -> [String] {
         return self.map{ $0.trimmingCharacters(in: .whitespacesAndNewlines) }.filter { !$0.isEmpty }
     }
 }

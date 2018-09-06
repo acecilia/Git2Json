1020c657faa62b7e0439f5a3201297e8b306f680
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-09-06 15:04:32 +0200

    Added XCodeReporter

--- Sources/Git2JsonCore/Helpers/XCodeReporter.swift
+++ Sources/Git2JsonCore/Helpers/XCodeReporter.swift
@@ -1,12 +1,50 @@
-/*public struct XcodeReporter {
-    internal static func generateForSingleViolation(_ violation: StyleViolation) -> String {
-        // {full_path_to_file}{:line}{:character}: {error,warning}: {content}
-        return [
-            "\(violation.location): ",
-            "\(violation.severity.rawValue): ",
-            "\(violation.ruleDescription.name) Violation: ",
-            violation.reason,
-            " (\(violation.ruleDescription.identifier))"
-            ].joined()
+import Foundation
+
+public enum Severity: String {
+    case warning
+    case error
+}
+
+struct Location: CustomStringConvertible {
+    let file: String?
+    let line: Int?
+    let character: Int?
+
+    init(file: String? = nil, line: Int? = nil, character: Int? = nil) {
+        self.file = file
+        self.line = line
+        self.character = character
     }
-}*/
+
+    var description: String {
+        // Xcode likes warnings and errors in the following format:
+        // {full_path_to_file}{:line}{:character}: {error,warning}: {content}
+        let fileString: String = file ?? "<nopath>"
+        let lineString: String = ":\(line ?? 1)"
+        let charString: String = character.map({ ":\($0)" }) ?? ""
+        return [fileString, lineString, charString].joined()
+    }
+}
+
+public struct XcodeReporter {
+    public static func print(
+        _ severity: Severity,
+        file: String? = nil,
+        line: Int? = nil,
+        character: Int? = nil,
+        message: String,
+        exitOnError: Bool = true
+        ) {
+        let location = Location(file: file, line: line, character: character)
+        let string = [
+            "\(location):",
+            "\(severity.rawValue):",
+            "(\(message))"
+            ].joined(separator: " ")
+        Swift.print(string)
+
+        if exitOnError && severity == .error {
+            exit(1)
+        }
+    }
+}

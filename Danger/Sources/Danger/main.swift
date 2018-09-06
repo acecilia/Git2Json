17893ccd8dcfa30167f939ad20ea818a7f5a9488
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-09-06 15:55:43 +0200

    Fix

--- Danger/Sources/Danger/main.swift
+++ Danger/Sources/Danger/main.swift
@@ -2,18 +2,19 @@ import Foundation
 import Git2JsonCore
 
 // xcrun --sdk macosx swift run -C Danger -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13"
+
 let diff = try Git.diff(.against(target: "origin/master")).run()
 let gitAbsoluteTopLevel = Git.absoluteTopLevel.run()
 
 for (index, change) in diff.enumerated() {
-    Script.print(.warning, file: "\(gitAbsoluteTopLevel)/\(change.path)", "index \(index), status: \(change.status)")
+    Reporter.print(.warning, file: "\(gitAbsoluteTopLevel)/\(change.path)", "index \(index), status: \(change.status)")
 }
-Script.print(.warning, file: "/Users/andres/Git/Git2Json/Sources/Git2JsonCore/Sources/Git/Git.swift", "Changes count: \(diff.count)")
+Reporter.print(.warning, file: "/Users/andres/Git/Git2Json/Sources/Git2JsonCore/Sources/Git/Git.swift", "Changes count: \(diff.count)")
 
 // Changelog check
 let changelogFileName = "CHANGELOG.md"
 if !diff.modifiedFiles.contains{ $0 == changelogFileName } {
-    Script.print(.error, file: "\(gitAbsoluteTopLevel)/\(changelogFileName)", "Changes are made, but no changelog entry has been found")
+    Reporter.print(.error, file: "\(gitAbsoluteTopLevel)/\(changelogFileName)", "Changes are made, but no changelog entry has been found")
 }
 
-Script.exit()
+Reporter.exit()

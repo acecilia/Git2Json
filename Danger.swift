e86f41273f3cbc981aae96fef2d0ce785e74b2f1
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-09-06 04:22:51 +0200

    Fixes

--- Danger.swift
+++ Danger.swift
@@ -1,8 +1,8 @@
 import Foundation
-import Git2JsonCore
+import Git2JsonCore // marathon:https://github.com/acecilia/Git2Json.git
 
 let diff = Git.diff(.against(target: "origin/master"))
-print(try diff.run().count)
+print(try diff.run().additions)
 print("/Users/andres/Git/Git2Json/Sources/Git2JsonCore/Sources/Git:1:: error: uu")
 exit(1)
 

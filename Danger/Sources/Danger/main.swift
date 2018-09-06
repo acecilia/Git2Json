8dc95efeb451a0573754930f212595cf4503a95e
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-09-06 06:26:04 +0200

    Clean

--- Danger/Sources/Danger/main.swift
+++ Danger/Sources/Danger/main.swift
@@ -1,14 +1,12 @@
 import Foundation
 import Git2JsonCore
 
-let diffCmd = Git.diff(.against(target: "origin/master"))
-let diff = try diffCmd.run()
-
-let gitAbsoluteTopLevel = Git.absoluteTopLevel.rawOutput()
+let diff = try Git.diff(.against(target: "origin/master")).run()
+let gitAbsoluteTopLevel = Git.absoluteTopLevel.run()
 
 for (index, change) in diff.enumerated() {
-    print("\(gitAbsoluteTopLevel)/\(change.path):7:: warning: index \(index), status: \(change.status)")
+    print("\(gitAbsoluteTopLevel)/\(change.path):5:: warning: index \(index), status: \(change.status)")
 }
-print("/Users/andres/Git/Git2Json/Sources/Git2JsonCore/Sources/Git/Git.swift:7:: warning: changes count: \(diff.count)")
+print("/Users/andres/Git/Git2Json/Sources/Git2JsonCore/Sources/Git/Git.swift:7:3: warning: changes count: \(diff.count)")
 exit(0)
 

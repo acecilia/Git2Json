db40bc505037b360a962988bbd946ae8effc6877
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-09-06 14:44:35 +0200

    Testing changelog check

--- Danger/Sources/Danger/main.swift
+++ Danger/Sources/Danger/main.swift
@@ -5,8 +5,14 @@ let diff = try Git.diff(.against(target: "origin/master")).run()
 let gitAbsoluteTopLevel = Git.absoluteTopLevel.run()
 
 for (index, change) in diff.enumerated() {
-    print("\(gitAbsoluteTopLevel)/\(change.path):5:: warning: index \(index), status: \(change.status)")
+    print("\(gitAbsoluteTopLevel)/\(change.path):1:: warning: index \(index), status: \(change.status)")
 }
 print("/Users/andres/Git/Git2Json/Sources/Git2JsonCore/Sources/Git/Git.swift:7:3: warning: changes count: \(diff.count)")
+
+// Changelog check
+let changelogFileName = "CHANGELOG.md"
+if !diff.modifiedFiles.contains{ $0 == changelogFileName } {
+    print("\(gitAbsoluteTopLevel)/\(changelogFileName):1:: error: changes are made, but no changelog entry has been found")
+}
 exit(0)
 

ac53653ddd9221b360b78b2d3cf9b5eedf13fcee
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-09-17 21:00:51 +0200

    fixes

--- Danger/Sources/Danger/main.swift
+++ Danger/Sources/Danger/main.swift
@@ -9,7 +9,11 @@ let diff = try Git.diff(.against(target: "origin/master")).run()
 for (index, change) in diff.enumerated() {
     Reporter.print(.warning, file: "\(change.path)", "index \(index), status: \(change.status)")
 }
-Reporter.print(.warning, file: "", "Changes count: \(diff.count)")
+Reporter.print(.warning, "Changes count: \(diff.count)")
+
+// {full_path_to_file}{:line}{:character}: {error,warning}: {content}
+print("/Users/andres/Git/Git2Json/Sources/Git2Json/Sources/Git/Git.swift:6:3: error: uu")
+
 
 // Changelog check
 let changelogFileName = "CHANGELOG.md"
--- Sources/Git2Json/Sources/Reporter/Reporter.swift
+++ Sources/Git2Json/Sources/Reporter/Reporter.swift
@@ -1,28 +1,11 @@
 import Foundation
 
-public var violations: [Violation] {
-    get { return Reporter.shared.violations }
-    set {Reporter.shared.violations = newValue }
-}
+public var reporter = Reporter()
 
-public func print(
-    _ severity: Severity,
-    file: String? = nil,
-    line: Int? = nil,
-    character: Int? = nil,
-    _ message: String
-    ) {
-    Reporter.shared.print(severity, file: file, line: line, character: character, message)
-}
+public struct Reporter {
+    public var violations: [Violation] = []
 
-public func exit(failOnError: Bool = true) {
-    Reporter.shared.exit(failOnError: failOnError)
-}
-
-private struct Reporter {
-    var violations: [Violation] = []
-
-    mutating func print(
+    public mutating func print(
         _ severity: Severity,
         file: String? = nil,
         line: Int? = nil,
@@ -35,14 +18,10 @@ private struct Reporter {
         Swift.print(violation)
     }
 
-    func exit(failOnError: Bool = true) {
-        if failOnError && violations.contains{ $0.severity == .error } {
+    public func finish(failOnError: Bool = true) {
+        if failOnError && violations.contains { $0.severity == .error } {
             Foundation.exit(1)
         }
         Foundation.exit(0)
     }
-}
-
-extension Reporter {
-    static var shared = Reporter()
 }
